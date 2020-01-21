f1a92575de892e03dd33ac8dd714e5d29063d0e8
qqbuby <qqbuby@gmail.com>
2020-01-21 11:33:00 +0800

    Add GA & GS tracking

---
+++ js/gridsum-webdissector-tracking.js
@@ -0,0 +1,15 @@
+{% if jekyll.environment == "production" %}
+<!-- Gridsum tracking code begin. -->
+<script type='text/javascript'>
+var _gsq = _gsq || [];
+    (function () {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'https://static.gridsumdissector.com/js/Clients/GWD-005120-9838EC/gs.js';
+        var firstScript = document.getElementsByTagName('script')[0];
+        firstScript.parentNode.insertBefore(s, firstScript);
+    })();
+</script>
+<!--Gridsum tracking code end. -->
+{% endif %}
